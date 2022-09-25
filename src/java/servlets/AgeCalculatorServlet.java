2fc6f1ccc72e36481c94c8b68a5531932d570202
marce <marce@Marcel_Lee>
2022-09-25 04:53:30 -0600

    Age Calculator is working

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,37 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marce
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       String strAge = request.getParameter("age");
+
+       request.setAttribute("age", strAge);
+
+
+       int nextAge = Integer.parseInt(strAge) + 1;
+
+       request.setAttribute("valid", "Your age next birthday will be " + nextAge);
+       getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+
+    }
+}
